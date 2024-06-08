2759975f150dee584b6211ffa24826abf2694e89
Mheligne23 <ignemhel08@gmail.com>
2024-06-08 17:20:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,17 @@ function clickDisLike2(){
   countDisLike2.textContent = totalDisLikes.toString()
 }  
 btnDisLike2.addEventListener("click",clickDisLike2)
+
+const submit = document.getElementById("submit")
+
+const comment = document.getElementById("comment")
+
+const commentbox = document.getElementById("commentbox")
+
+function submitComment(){
+
+commentbox.textContent += comment.value.toString() + "\n"
+
+}
+
+submit.addEventListener("click", submitComment)
